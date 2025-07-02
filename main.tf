09f0cef9a1058978ca508484f8f5e27dc87c8742
sammycastl <nsmartinezcastl@gmail.com>
2025-07-01 22:33:57 -0600

    fix

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
 provider "azurerm" {
-  subscription_id = var.subscription_id
   features {}
+  subscription_id = var.subscription_id
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "rg-${var.project}-${var.environment}"
 }
 
 resource "azurerm_resource_group" "rg" {
+  count    = var.create_resource_group ? 1 : 0
   name     = "rg-${var.project}-${var.environment}"
   location = var.location
   tags     = var.tags
