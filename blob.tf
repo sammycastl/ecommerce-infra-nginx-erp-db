a6d1d99c8698d91b0316d5b16abae1727a985cbd
carlos <carlosftru@gmail.com>
2025-07-03 02:05:45 -0600

    final terraform code

--- blob.tf
+++ blob.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "ecommercesa" {
   name                     = "ecommercesa${lower(var.project)}${lower(var.environment)}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
--- erp.tf
+++ erp.tf
@@ -1,21 +1,21 @@
 resource "azurerm_virtual_network" "erp_vnet" {
   name                = "erp-vnet-${var.project}-${var.environment}"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "erp_subnet" {
   name                 = "erp-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.erp_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "erp1_nic" {
   name                = "erp1-nic-${var.project}-${var.environment}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -26,8 +26,8 @@ resource "azurerm_network_interface" "erp1_nic" {
 
 resource "azurerm_linux_virtual_machine" "erp1" {
   name                = "erp1-${var.project}-${var.environment}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   size                = "Standard_B2s"
   admin_username      = "adminuser"
   network_interface_ids = [azurerm_network_interface.erp1_nic.id]
@@ -44,8 +44,8 @@ resource "azurerm_linux_virtual_machine" "erp1" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "20.04-LTS" 
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server" 
     version   = "latest"
   }
 
