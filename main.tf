8483b424c47e85f30a083f1c0c2561e44d2c13f8
sammycastl <nsmartinezcastl@gmail.com>
2025-07-03 23:36:49 -0600

    main

--- main.tf
+++ main.tf
@@ -6,10 +6,3 @@ provider "azurerm" {
 data "azurerm_resource_group" "rg" {
   name = "rg-${var.project}-${var.environment}"
 }
-
-resource "azurerm_resource_group" "rg" {
-  count    = var.create_resource_group ? 1 : 0
-  name     = "rg-${var.project}-${var.environment}"
-  location = var.location
-  tags     = var.tags
-}
